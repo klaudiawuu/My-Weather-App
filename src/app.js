56c0feecefcf6ca87daed22c4cde4f03e8db5b58
klaudiawuu <klaudia.wawrzynczyk@gmail.com>
2022-04-01 15:05:38 +0200

    date and time updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
+function actualDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
-  console.log(response.data);
 
   let city = document.querySelector("h1");
   city.innerHTML = response.data.name;
@@ -18,11 +44,12 @@ function displayTemp(response) {
 
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
+
+  let dayTime = document.querySelector("#day");
+  dayTime.innerHTML = actualDate(response.data.dt * 1000);
 }
 
 let apiKey = "a367566821d5256a1c920a360eab8e9e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemp);
